36b9e5f35c90b63e25b74fffad90ddf8104c9424
troubleskiller <troubleskiller@outlook.com>
2022-09-28 21:16:09 +0800

    solve task 453 最小操作次数使元素相等

--- bin/leetcode_tech.dart
+++ bin/leetcode_tech.dart
@@ -1,23 +1,9 @@
-import 'common_class/common.dart';
-import 'medium/143.dart';
-
 void main(List<String> arguments) {
   ///do this solving in 9/26,hard to understand why these people from leetcode can have so smart brain.Also i want to know the reason why the world's best programing languages can be so hard to be used in leetcode.
   // print(isPalindrome(1429669241)); //---->>> 09.dart => 回文数（不允许使用字符串）
 
-  reverserNode(ListNode(
-      0,
-      ListNode(
-          1,
-          ListNode(
-              3,
-              ListNode(
-                  4,
-                  ListNode(
-                      5,
-                      ListNode(
-                          6,
-                          ListNode(
-                            7,
-                          ))))))));
+  //do these two solving in 9/27,反转链表比第一次要熟练一点，虽然还是没办法一遍写出来答案，但是看答案时有种一下子就明白的感觉，希望下次再写的时候，可以熟练很多。 024 => 反转链表
+  //关于快慢指针找出中间点的solution，感觉也很熟悉，可能是因为太久没刷题，所以出现了问题。 143 => 重排链表，先后再前。
+
+  //do 453 => 最小操作次数使数组元素相等 in 9/28,属于脑筋急转弯类型的题目，不过思路很巧妙啦。
 }
