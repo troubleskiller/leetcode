8318498571274014bedf84bd68f2c862146acd91
troubleskiller <troubleskiller@outlook.com>
2022-09-28 20:43:51 +0800

    update TreeNode structure

--- bin/common_class/common.dart
+++ bin/common_class/common.dart
@@ -3,3 +3,10 @@ class ListNode {
   ListNode? next;
   ListNode([this.val = 0, this.next]);
 }
+
+class TreeNode {
+  int val;
+  TreeNode? left;
+  TreeNode? right;
+  TreeNode([this.val = 0, this.left, this.right]);
+}
